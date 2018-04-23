06907a49cbcda7d6edb64ca9e67d33bfd24386a0
nx111 <gd.zhangdz@gmail.com>
2018-04-23 23:05:19 +0800

    cpufreq: add governor smartass2.
    Change-Id: Iccc05f76773ad770dfb5c3120aa511a012aed788

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -482,6 +482,9 @@ extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_KTOONSERVATIVE)
 extern struct cpufreq_governor cpufreq_gov_ktoonservative;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_ktoonservative)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
+extern struct cpufreq_governor cpufreq_gov_smartass2;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_smartass2)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
 extern struct cpufreq_governor cpufreq_gov_intellidemand;
 #define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_INTELLIDEMAND)
