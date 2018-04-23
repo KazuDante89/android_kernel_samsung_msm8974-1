3b9e286d60fefaf73e1ba016023cdb73cb6a139c
nx111 <gd.zhangdz@gmail.com>
2018-04-23 23:11:25 +0800

    cpufreq: add governor lionheart.
    Change-Id: I1b9409531559d3a21a203cb6a1d86e99b8503665

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -497,6 +497,9 @@ extern struct cpufreq_governor cpufreq_gov_abyssplugv2;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ)
 extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_pegasusq)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONHEART)
+extern struct cpufreq_governor cpufreq_gov_lionheart;
+#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_LIONHEART)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
 extern struct cpufreq_governor cpufreq_gov_smartmax;
 #define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_SMARTMAX)
