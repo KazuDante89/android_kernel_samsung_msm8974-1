6d8aa427d1f2052c495c063067d9123643d9bf64
nx111 <gd.zhangdz@gmail.com>
2018-04-23 23:02:55 +0800

    cpufreq: add governor hyper.
    Change-Id: I811733c750c8f2b2f7aea66bbb63c971eb8aa7fb

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -488,6 +488,9 @@ extern struct cpufreq_governor cpufreq_gov_intellidemand;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ)
 extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_pegasusq)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HYPER)
+extern struct cpufreq_governor cpufreq_gov_hyper;
+#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_hyper)
 #endif
 
 
